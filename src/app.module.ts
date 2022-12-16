c0647446f7686f90568906526e5b579d94f06b64
Diego Fernandes <diego.schell.f@gmail.com>
2022-12-15 21:20:48 -0300

    Kafka

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
+import { MessagingModule } from '@infra/messaging/messaging.module';
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from './infra/database/database.module';
 import { HttpModule } from './infra/http/http.module';
 
 @Module({
-  imports: [HttpModule, DatabaseModule],
+  imports: [HttpModule, DatabaseModule, MessagingModule],
 })
 export class AppModule {}
